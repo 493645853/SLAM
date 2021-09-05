f05d596e72a3b934c293c481bfc478eaf83f5e88
493645853 <you@example.co & 'c:\Users\86180\.vscode\extensions\ms-vscode.cpptools-1.6.0\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-ixo2nlhm.xgk' '--stdout=Microsoft-MIEngine-Out-difmocxt.r1c' '--stderr=Microsoft-MIEngine-Error-u2emdc0m.l4s' '--pid=Microsoft-MIEngine-Pid-z3mhukb5.boe' '--dbgExe=D:\mingw-w64\x86_64-8.1.0-win32-seh-rt_v6-rev0\mingw64\bin\gdb.exe' '--interpreter=mi' m>
2021-09-05 23:06:08 +0100

    Successfully configure OpenCV using CMAKE

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
+#include<opencv2/core.hpp>
+#include<opencv2/videoio.hpp>
 #include<opencv2/opencv.hpp>
-using namespace std;
+#include<opencv2/highgui.hpp>
+#include<opencv2/features2d.hpp>
 
-int main(){
-    cout<<"main loop"<<endl;
-    while(1){}
+int main( int argc, char** argv )
+{
+    cv::Mat frame;
+    //---INITIALIZE VIDEOCAPTURE
+    cv::VideoCapture cap;
+    cap.open(0,cv::CAP_ANY);
+    if(!cap.isOpened())
+    {
+        return -1;
+    }
+
+    while(1)
+    {
+        cap.read(frame);
+        cv::imshow("Camera",frame);
+        if(cv::waitKey(5)>=0)
+        {
+            break;
+        }
+    }
+
     return 0;
 }
