3c60d4ee2de0c34611c858dbba7bb782511e942f
493645853 <you@example.co & 'c:\Users\86180\.vscode\extensions\ms-vscode.cpptools-1.6.0\debugAdapters\bin\WindowsDebugLauncher.exe' '--stdin=Microsoft-MIEngine-In-ixo2nlhm.xgk' '--stdout=Microsoft-MIEngine-Out-difmocxt.r1c' '--stderr=Microsoft-MIEngine-Error-u2emdc0m.l4s' '--pid=Microsoft-MIEngine-Pid-z3mhukb5.boe' '--dbgExe=D:\mingw-w64\x86_64-8.1.0-win32-seh-rt_v6-rev0\mingw64\bin\gdb.exe' '--interpreter=mi' m>
2021-09-05 19:47:35 +0100

    Create OpenCV project

---
+++ include/basic_object.h
@@ -0,0 +1,14 @@
+#ifndef __BASIC_OBJECT_H__
+#define __BASIC_OBJECT_H__
+
+
+class basic_object
+{
+private:
+    basic_object();
+    ~basic_object();
+
+public:
+    void doSth();
+};
+#endif // __BASIC_OBJECT_H__
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace std;
+
+int main(){
+    cout<<"main loop"<<endl;
+    while(1){}
+    return 0;
+}
